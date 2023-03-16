f047f818987abd62784494f6266e6be625d7096e
situodys <situodys@gmail.com>
2023-03-16 17:11:40 +0900

    init project

---
+++ src/test/java/kw/soft/gourmet/GourmetApplicationTests.java
@@ -0,0 +1,13 @@
+package kw.soft.gourmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
